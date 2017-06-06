7d835f993d95bf99e9e75cf6774ac2ea0a50c401
Jethro Lising <lising.jg@gmail.com>
2017-06-06 14:35:04 -0400

    Ok

--- wiki/all.go
+++ wiki/all.go
@@ -1,6 +1,7 @@
 package wiki
 
 import (
+	"fmt"
 	"github.com/jg-l/wiki/db"
 	"github.com/russross/blackfriday"
 	"github.com/zenazn/goji/web"
@@ -23,43 +24,5 @@ func (w *Wiki) All(c web.C, rw http.ResponseWriter, r *http.Request) {
 
 		return nil
 
-		if err != nil {
-			p.Text = []byte(emptyPageString)
-			//p.Created = []byte(y)
-		}
-
-		vars := map[string]interface{}{
-			"Path":     "/" + string(p.Name) + "/edit",
-			"Name":     BytesAsHTML(p.Name),
-			"Text":     BytesAsHTML(ParsedMarkdown(p.Text)),
-			"Created":  BytesAsTime(p.Created),
-			"Modified": BytesAsTime(p.Modified),
-		}
-
-		t := template.Must(template.New("show").Parse(showTpl))
-		t.Execute(rw, vars)
-
-		return nil
 	})
-}
-
-// RedirectToShow redirects to the show endpoint using a HTTP 302
-func (w *Wiki) RedirectToShow(c web.C, rw http.ResponseWriter, r *http.Request) {
-	http.Redirect(rw, r, "/"+c.URLParams["name"], 302)
-}
-
-func BytesAsTime(b []byte) *time.Time {
-	v := new(time.Time)
-	v.UnmarshalBinary(b)
-	return v
-}
-
-// BytesAsHTML returns the template bytes as HTML
-func BytesAsHTML(b []byte) template.HTML {
-	return template.HTML(string(b))
-}
-
-// ParsedMarkdown returns provided bytes parsed as Markdown
-func ParsedMarkdown(b []byte) []byte {
-	return blackfriday.MarkdownCommon(b)
 }
